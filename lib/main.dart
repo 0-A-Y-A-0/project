32058aad737a7039f4816ec68474b1abe89b83d3
Aya Hamad <aya.hamad.00000@gmail.com>
2025-12-13 00:53:20 +0300

    (●'◡'●)

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: watashiWaSta()));
+}
+
+class watashiWaSta extends StatelessWidget {
+  const watashiWaSta({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
